8d054ace0e67c0cad4bf350e924af1a898eb8a1c
Christopher Lien <cnlien@gmail.com>
2019-11-04 13:55:54 -0500

    first commit

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Card} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Card>Github User card</Card>
+    </div>
+  );
+}
+
+export default App;
