001928103eac84f353dcc7496c42cd5cb6349281
Christopher Lien <cnlien@gmail.com>
2019-11-04 16:44:40 -0500

    Component Did Mount and API call to github rendering out my own card is complete

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { Card } from 'reactstrap'
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import MyCard from './components/myCard';
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+        admindata: {}
+    }
+  }
+
+  componentDidMount () {
+      console.log("Compenent Did Mount")
+      fetch ("https://api.github.com/users/cnlien")
+          .then(res => res.json())
+          .then(mydata => this.setState({admindata: mydata}))
+          .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className = "cardContainer">
+
+        <div className="my-card"> 
+          <MyCard
+            avatar_url = {this.state.admindata.avatar_url}
+            name = {this.state.admindata.name}
+            login = {this.state.admindata.login}
+            bio = {this.state.admindata.bio}
+          />
+        </div>
+
+        <div className="friend-cards">
+          <MyCard />
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ react-github-user-card/src/components/myCard.js
@@ -0,0 +1,20 @@
+// import React, { Component } from 'react';
+import React from 'react';
+import { Card, CardImg, CardTitle, CardBody, CardText, CardLink } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const MyCard = props => {
+
+    return (
+        <Card>
+            <CardImg top width="100%" src={props.avatar_url} />
+            <CardBody>
+                <CardTitle>{props.name}</CardTitle>
+                <CardLink > {props.login}</CardLink>
+                <CardText>{props.bio}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default MyCard;
